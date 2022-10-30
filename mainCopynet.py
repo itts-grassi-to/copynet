a14a2696904ca0d45dd1c58eb15e28dec185f635
Daniele Ortu <daniele.ortu@itisgrassi.edu.it>
2022-10-30 10:42:08 +0100

    corpo della copia

--- mainCopynet.py
+++ mainCopynet.py
@@ -1,6 +1,9 @@
 import gi
 import os
 import csv
+import time
+import threading
+from threading import *
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -19,6 +22,8 @@ class EventiMain:
         self.__cpn.on_btHelp_clicked(self.__win)
     def on_btAvvia_clicked(self, bt):
         self.__cpn.on_avvia()
+    def on_btStop_clicked(self, bt):
+        self.__cpn.on_stop()
 class Dlg(Gtk.Dialog):
     def __init__(self, parent, testo) :
         super().__init__(title="Informazioni", transient_for=parent, flags=0)
@@ -33,6 +38,8 @@ class Dlg(Gtk.Dialog):
 
 class CPN():
     def __init__(self):
+        self.__semaLettoriScrittori = Semaphore(1)
+        self.__semaAntiRimbalzo = Semaphore(0)
         self.__caricaHosts()
     def __creaRigaHost(self, l, first):
         row = Gtk.ListBoxRow()
@@ -71,18 +78,65 @@ class CPN():
             for row in lettore:
                 lstHosts.add(self.__creaRigaHost(row, False))
                 # print(row)
+    def __thPerc(self):
+        self.__semaLettoriScrittori.acquire()
+        print("thPerc avviato")
+        self.__semaLettoriScrittori.release()
+        while not self.__stopThread:
+            lstHosts.show_all()
+            time.sleep(2)
+        print("thPerc fermato")
+    def __thCopia(self, row, sema):
+        self.__semaLettoriScrittori.acquire()
+        print("thCopia avviato")
+        self.__semaLettoriScrittori.release()
+        # row.get_child().get_children()[0]     # nome PC
+        # row.get_child().get_children()[1]     # indirizzo IP
+        # row.get_child().get_children()[3]     attivato
+        iperc = 0
+        while not self.__stopThread:
+            row.get_child().get_children()[2].set_label(str(iperc)+"%")       # percentuale
+            iperc = iperc + 1
+            time.sleep(1)
+            if iperc >= 20:
+                break
+        sema.release()
+        print("thCopia fermato")
+
+    def __thFerma(self, sema):
+        for s in sema:
+            s.acquire()
+        print("stop thread")
+        self.__stopThread = True
+        self.__semaAntiRimbalzo.acquire(False)
     def on_btHelp_clicked(self, win):
         dialog = Dlg(win, "Il software è costruito\nda Ortu prof. Daniele\nemail: daniele.ortu@itisgrassi.edu.it")
         dialog.run()
         dialog.destroy()
     def on_avvia(self):
         #print("avvia")
+        if self.__semaAntiRimbalzo.acquire(False):
+            self.__semaAntiRimbalzo.release()
+            return
+        print("***********")
+        self.__semaAntiRimbalzo.release()
+        self.__stopThread = False
         i = 1
         row = lstHosts.get_row_at_index(i)
+        if row != None:
+            threading.Thread(target=self.__thPerc, args=()).start()
+        sema = []
         while row != None:
+            sema.append( Semaphore(0) )
+            # sema[len(sema) - 1].acquire()
+            threading.Thread(target=self.__thCopia, args=(row, sema[len(sema)-1] )).start()
             i = i+1
             row = lstHosts.get_row_at_index(i)
-            print(i)
+            # print(i)
+        threading.Thread(target=self.__thFerma, args=(sema, )).start()
+    def on_stop(self):
+        self.__stopThread = True
+
 
 # *********** MAIN
 builMain = Gtk.Builder()
